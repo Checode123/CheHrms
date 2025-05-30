349dffbb0c75966b3890d7f99fc9436e2f1f9f12
kanishka <gkanishka2004@gmail.com>
2025-05-30 11:40:59 +0530

    supabase integrated. index.js file setup.

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+
+
 
 app.use(cors({
   origin: 'http://localhost:5173'
